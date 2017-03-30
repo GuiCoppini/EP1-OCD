21e26ec264e29a64fdad4d34bb44279a9fb69213
Guilherme Coppini <guilherme.pavani@bidu.com.br>
2017-03-30 11:04:40 -0300

    Add min method

--- Conversoes.java
+++ Conversoes.java
@@ -1,6 +1,11 @@
 public class Conversoes {
-    public static max(int a, int b){
+    public static int max(int a, int b){
         if(a>b) return a;
+        return b;
+    }
+
+    public static int min(int a, int b){
+        if(a<b) return a;
         return b;
     }
 }
