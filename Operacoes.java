576e311c5d1439bbe95f67f34be49a10ef9c7f67
Lucas Capalbo <lucas.capalbo@usp.br>
2017-04-03 14:58:06 -0300

    novo metodo soma
    melhor, consegue fazer somas entre quaisquer 2 binários, pois na versão
    antiga dava problema ao serem de tamanhos (bits) diferentes. e novo
    método para converter decimal para binário, usando o “toBynaryString”,
    do próprio java

--- Operacoes.java
+++ Operacoes.java
@@ -1,31 +1,9 @@
 public class Operacoes extends Conversoes {
     
     int[] somaBinarios(int[] bin1, int[] bin2) {
-        int[] binarioCorrigido, resultadoSoma;
-        if (max(bin1.length, bin2.length) == bin1.length) {
-            binarioCorrigido = new int[bin1.length];
-            for (int i = bin2.length - 1; i > 0; i--) {
-                binarioCorrigido[i] = bin2[i];
-            }
-            int[] resultado = new int[binarioCorrigido.length + 1];
-            for (int i = resultado.length - 1; i > 0; i--) {
-                resultado[i] += bin1[i - 1] + binarioCorrigido[i - 1];
-                if (resultado[i] >= 2) {
-                    resultado[i - 1] = resultado[i] / 2;
-                    resultado[i] = resultado[i] % 2;
-                }
-            }
-            resultadoSoma = new int[resultado.length];
-            for (int j = 0; j < resultado.length; j++) {
-                resultadoSoma[j] = resultado[j];
-            }
-            
-        } else {
-            binarioCorrigido = new int[bin2.length];
-            for (int i = bin1.length - 1; i > 0; i--) {
-                binarioCorrigido[i] = bin1[i];
-            }
-            int[] resultado = new int[binarioCorrigido.length + 1];
+        int[] binarioCorrigido, resultado;
+        if (bin1.length == bin2.length) {
+            resultado = new int[bin1.length + 1];
             for (int i = resultado.length - 1; i > 0; i--) {
                 resultado[i] += bin1[i - 1] + bin2[i - 1];
                 if (resultado[i] >= 2) {
@@ -33,18 +11,44 @@ public class Operacoes extends Conversoes {
                     resultado[i] = resultado[i] % 2;
                 }
             }
-            resultadoSoma = new int[resultado.length];
-            for (int j = 0; j < resultado.length; j++) {
-                resultadoSoma[j] = resultado[j];
-                
+        } else if (max(bin1.length, bin2.length) == bin1.length) {
+            binarioCorrigido = new int[bin1.length];
+            int j = binarioCorrigido.length - 1;
+            for (int i = bin2.length - 1; i >= 0; i--) {
+                binarioCorrigido[j] = bin2[i];
+                j--;
+            }
+            resultado = new int[binarioCorrigido.length + 1];
+            for (int i = resultado.length - 1; i > 0; i--) {
+                resultado[i] += bin1[i - 1] + binarioCorrigido[i - 1];
+                if (resultado[i] >= 2) {
+                    resultado[i - 1] = resultado[i] / 2;
+                    resultado[i] = resultado[i] % 2;
+                }
+            }
+        } else {
+            binarioCorrigido = new int[bin2.length];
+            int j = binarioCorrigido.length - 1;
+            for (int i = bin1.length - 1; i >= 0; i--) {
+                binarioCorrigido[j] = bin1[i];
+                j--;
+            }
+            resultado = new int[binarioCorrigido.length + 1];
+            for (int i = resultado.length - 1; i > 0; i--) {
+                resultado[i] += binarioCorrigido[i - 1] + bin2[i - 1];
+                if (resultado[i] >= 2) {
+                    resultado[i - 1] = resultado[i] / 2;
+                    resultado[i] = resultado[i] % 2;
+                }
             }
         }
-        return resultadoSoma;
-    }    
-    int[] converteParaBinario(int decimal) {
-        return null;
+        return resultado;
     }
     
+    int[] converteParaBinario(int decimal) {
+        String binaria = Integer.toBinaryString(decimal);
+        return converteString(binaria);
+    }    
     int[] converteString(String numero1) {
         String conversao[] = numero1.split(" ");
         int[] binario = new int[conversao.length];
