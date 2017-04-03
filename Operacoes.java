d88ed361f24150d501825e7923ff09357224f703
Lucas Capalbo <lucas.capalbo@usp.br>
2017-04-03 13:51:01 -0300

    novo Soma binario, e imprime Binario
    Agora o metodo soma, consegue manusear binários de diferentes tamanho
    (exemplo 1 0 0 1, e 1 0), etc.
    o metodo imprime também foi criado.

--- Operacoes.java
+++ Operacoes.java
@@ -1,18 +1,46 @@
 public class Operacoes extends Conversoes {
     
     int[] somaBinarios(int[] bin1, int[] bin2) {
-        int[] resultado = new int[bin1.length + 1];
-        for (int i = resultado.length - 1; i > 0; i--) {
-            resultado[i] += bin1[i - 1] + bin2[i - 1];
-            if (resultado[i] >= 2) {
-                resultado[i - 1] = resultado[i] / 2;
-                resultado[i] = resultado[i] % 2;
+        int[] binarioCorrigido, resultadoSoma;
+        if (max(bin1.length, bin2.length) == bin1.length) {
+            binarioCorrigido = new int[bin1.length];
+            for (int i = bin2.length - 1; i > 0; i--) {
+                binarioCorrigido[i] = bin2[i];
+            }
+            int[] resultado = new int[binarioCorrigido.length + 1];
+            for (int i = resultado.length - 1; i > 0; i--) {
+                resultado[i] += bin1[i - 1] + binarioCorrigido[i - 1];
+                if (resultado[i] >= 2) {
+                    resultado[i - 1] = resultado[i] / 2;
+                    resultado[i] = resultado[i] % 2;
+                }
+            }
+            resultadoSoma = new int[resultado.length];
+            for (int j = 0; j < resultado.length; j++) {
+                resultadoSoma[j] = resultado[j];
+            }
+            
+        } else {
+            binarioCorrigido = new int[bin2.length];
+            for (int i = bin1.length - 1; i > 0; i--) {
+                binarioCorrigido[i] = bin1[i];
+            }
+            int[] resultado = new int[binarioCorrigido.length + 1];
+            for (int i = resultado.length - 1; i > 0; i--) {
+                resultado[i] += bin1[i - 1] + bin2[i - 1];
+                if (resultado[i] >= 2) {
+                    resultado[i - 1] = resultado[i] / 2;
+                    resultado[i] = resultado[i] % 2;
+                }
+            }
+            resultadoSoma = new int[resultado.length];
+            for (int j = 0; j < resultado.length; j++) {
+                resultadoSoma[j] = resultado[j];
+                
             }
         }
-        
-        return resultado;
-    }
-    
+        return resultadoSoma;
+    }    
     int[] converteParaBinario(int decimal) {
         return null;
     }
@@ -24,5 +52,10 @@ public class Operacoes extends Conversoes {
             binario[i] = Integer.parseInt(conversao[i]);
         }
         return binario;
+    }
+    void imprimeBinario(int[] binario){
+        for (int i = 1; i < binario.length; i++) {
+            System.out.println(binario[i]);
+        }
     }
 }
